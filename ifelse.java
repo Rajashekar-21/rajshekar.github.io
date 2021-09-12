da53ab4b2650ba7996f2d2ea66c58074c80ab7d7
Rajashekar-21 <88504434+Rajashekar-21@users.noreply.github.com>
2021-09-12 20:40:31 +0530

    assignment-2

---
+++ ifelse.java
@@ -0,0 +1,15 @@
+
+public class ifelse {
+
+	public static void main(String[] args) {
+	    short x = 9;
+	    short y = 5;
+	    if(x>y) {
+	    	System.out.println("x is greater");
+	    }
+	    else {
+	    	System.out.println("y is greater");
+	    }
+	}
+
+}
